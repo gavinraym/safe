1edae22f36552924caf296bdd6772e2d0b5fd386
Gavin Ray <gavinraym@gmail.com>
2020-08-12 15:02:09 -0700

    Testing

--- lock.py
+++ lock.py
@@ -1,8 +1,9 @@
 from PIL import Image
+import os
 
 class Lock():
     def __init__(self):
-        self.combo = 'combo'
+        self._combo = 'combo'
 
     def __repr__(self):
         return '''This is a safe. It contains a very special item.
@@ -15,11 +16,11 @@ class Lock():
          '''
 
     def dial(self, combo):
-        self.combo = combo
+        self._combo = combo
 
     def handle(self):
         image = open('.safe_contents', 'rb').read()
-        key = self.combo*int(len(image)/4)
+        key = self._combo*int(len(image)/4)
         dcr_img = bytes([a ^ b for a, b in zip(image, key.encode('utf8'))])
         f = open('safe_contents.png', 'wb')
         f.write(dcr_img)
@@ -27,10 +28,8 @@ class Lock():
             Image.open('safe_contents.png').show()
             print('Safe unlocked!')
         except:
+            os.remove(f)
             print('Wrong code.')
 
     def backdoor(self):
         return 'https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c'
-
-    if __name__ == '__main__':
-        'The Lock class can be instatiated with: lock = Lock()'
